6132dca1f5936b2d7ef04830ce295415f32e5b42
victo <victo@LAPTOP-PK7AAICT>
2021-12-05 15:35:17 +0300

    Добавьте файлы проекта.

---
+++ frac/Fraction.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+class Fraction
+{
+private:
+	long numerator;
+	long denominator;
+public:
+	Fraction(long, long);
+	long reduction(long, long);
+	void setNumerator(long);
+	long getNumerator();
+	void setDenominator(long);
+	long getDenominator();
+	void check();
+	void print();
+	double getDouble();
+	bool operator > (Fraction);
+	bool operator < (Fraction);
+	bool operator >= (Fraction);
+	bool operator <= (Fraction);
+	bool operator == (Fraction);
+	bool operator != (Fraction);
+	Fraction operator * (Fraction);
+	Fraction operator + (Fraction);
+	Fraction operator - (Fraction);
+	Fraction operator / (Fraction);
+	
+};
+
